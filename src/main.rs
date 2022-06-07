0434bdfe7e0e12eccb41614910186b57c65ceaa6
Erin van der Veen <erin.vanderveen@tweag.io>
2022-06-07 12:29:23 +0200

    Contruct flat list of Atoms

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,34 @@
 use pretty::RcDoc;
 use std::{error::Error, fs, io};
-use tree_sitter::{Node, Parser, Query, QueryCursor};
+use tree_sitter::{Node, Parser, Query, QueryCursor, Range};
 use tree_sitter_json::language;
 
 static TEST_FILE: &str = "tests/json.json";
 static QUERY_FILE: &str = "languages/queries/json.scm";
+
+/// A Node from tree-sitter is turned into into a list of atoms
+#[derive(Debug)]
+enum Atom {
+    Leaf { content: String, range: Range },
+    Softline,
+    Hardline,
+    IndentStart,
+    IndentEnd,
+}
+
+//
+fn collect_leafs<'a>(node: Node, atoms: &mut Vec<Atom>, source: &'a [u8]) {
+    if node.child_count() == 0 {
+        atoms.push(Atom::Leaf {
+            content: String::from(node.utf8_text(source).unwrap()),
+            range: node.range(),
+        });
+    } else {
+        for child in node.children(&mut node.walk()) {
+            collect_leafs(child, atoms, source)
+        }
+    }
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
     // Read file
@@ -32,11 +56,11 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let matches = cursor.matches(&query, root, source);
 
-    for m in matches {
-        for c in m.captures {
-            println!("{:#?}", c.node.kind());
-        }
-    }
+    let mut atoms: Vec<Atom> = Vec::new();
+
+    collect_leafs(root, &mut atoms, source);
+
+    println!("{:#?}", atoms);
 
     Ok(())
 }
