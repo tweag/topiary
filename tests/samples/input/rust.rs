7bb54a619a3332efb09cfc3ad89569c144ce234b
Tor Hovland <tor.hovland@tweag.io>
2022-09-05 12:37:27 +0200

    Test inputs against their expected output.

--- tests/samples/input/rust.rs
+++ tests/samples/input/rust.rs
@@ -1,22 +1,35 @@
+
 enum OneLine { Leaf { content: String, id: usize, size: usize, }, Hardline { content: String, id: usize }, Space, }
 
 enum ExpandEnum { 
     Leaf { content: String, id: usize, size: usize, }, Hardline { content: String, id: usize },
     Space, 
 }
-
-enum NoFinalComma { 
-    Leaf { content: String, id: usize, size: usize, }, Hardline { content: String, id: usize },
+enum
+NoFinalComma { 
     Space
 }
 
-enum ExpandTwoLevels { 
-    Leaf { 
-        content: String, 
+
+
+   enum    ExpandTwoLevels    { 
+
+      Leaf    { 
+          content  :   String  ,   
         id: usize,
+
+
+
         size: usize,
-    },
-    Hardline { content: String, id: usize },
+    }  ,
+  Hardline{content:String,id:usize},
+
+
+
+    // comment between enum items
+
+
+
     Space, 
-}
+            }
 
