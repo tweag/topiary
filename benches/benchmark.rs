560e40ec965ba95aa1f259775fd60da6f45236eb
Tor Hovland <tor.hovland@tweag.io>
2022-09-15 08:29:45 +0200

    Read input file outside of benchmark.

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,15 +1,16 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion};
 use std::fs;
-use std::io::{BufReader, BufWriter};
+use std::io;
 use tree_sitter_formatter::{formatter, Language};
 
 fn criterion_benchmark(c: &mut Criterion) {
     let input_path = "tests/samples/input/ocaml.ml";
+    let input_content = fs::read_to_string(input_path).unwrap();
 
     c.bench_function("format ocaml", |b| {
         b.iter(|| {
-            let mut input = BufReader::new(fs::File::open(input_path).unwrap());
-            let mut output = BufWriter::new(Vec::new());
+            let mut input = input_content.as_bytes();
+            let mut output = io::BufWriter::new(Vec::new());
 
             formatter(&mut input, &mut output, Language::Ocaml)
         })
