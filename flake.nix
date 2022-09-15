ecbee48354dd530b90a4f50f54f5a372d3afd054
Tor Hovland <tor.hovland@tweag.io>
2022-09-15 11:55:43 +0200

    Cachix.

--- flake.nix
+++ flake.nix
@@ -1,6 +1,15 @@
 {
   description = "A general code formatter based on tree-sitter.";
 
+  nixConfig = {
+    extra-substituters = [
+      "https://tweag-tree-sitter-formatter.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "tweag-tree-sitter-formatter.cachix.org-1:R95oCa9JV/Cu8dtdFZY55HLFqJ3ASh34dXh7o7LeL5Y="
+    ];
+  };
+  
   inputs = {
     crane.url = "github:ipetkov/crane";
     flake-utils.follows = "crane/flake-utils";
