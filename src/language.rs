a3276b875fd148ceb72d6860a98ff0c8f3ffa507
Christopher Harrison <christopher.harrison@tweag.io>
2022-12-09 10:05:18 +0000

    Replaced Difftastic license with simple citations

--- src/language.rs
+++ src/language.rs
@@ -1,25 +1,3 @@
-// Portions of this code, highlighted below, are influenced by Difftastic (MIT licensed):
-//
-// Copyright (c) 2021-2022 Wilfred Hughes
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
-
 use std::path::{Path, PathBuf};
 
 use crate::{FormatterError, FormatterResult};
@@ -33,7 +11,8 @@ pub enum Language {
     Toml,
 }
 
-// NOTE Influenced by Difftastic; see above
+// NOTE This list of extension mappings is influenced by Wilfred Hughes' Difftastic
+// https://github.com/Wilfred/difftastic/blob/master/src/parse/guess_language.rs
 const EXTENSIONS: &[(&str, &[&str])] = &[
     (
         "json",
@@ -74,7 +53,8 @@ impl Language {
     }
 
     pub fn detect(filename: &str) -> FormatterResult<&str> {
-        // NOTE Influenced by Difftastic; see above
+        // NOTE This extension search is influenced by Wilfred Hughes' Difftastic
+        // https://github.com/Wilfred/difftastic/blob/master/src/parse/guess_language.rs
         if let Some(extension) = Path::new(filename).extension() {
             let extension = extension.to_str().unwrap();
 
