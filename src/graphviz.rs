9f21ff25820c43a260dfc054e114aaea92004d3f
Christopher Harrison <christopher.harrison@tweag.io>
2023-03-14 10:55:04 +0000

    Apply @torhovland's suggestions
    * String::new doesn't allocate, so we can remove the Option to make
      things simpler
    * When allocation is needed, reserve for the worst case, as the input is
      likely to be short
    * Tests for the Cow mechanism

--- src/graphviz.rs
+++ src/graphviz.rs
@@ -7,25 +7,25 @@ use crate::{tree_sitter::SyntaxNode, FormatterResult};
 /// We double-escape whitespace (\n and \t) so it is
 /// rendered as the escaped value in the GraphViz output
 fn escape(input: &str) -> Cow<str> {
-    let mut buffer: Option<String> = None;
+    // No allocation happens for an empty string
+    let mut buffer = String::new();
 
     let mut start: usize = 0;
     let length = input.len();
 
-    let append = |buffer: &mut Option<String>, from: &mut usize, to: usize, suffix: &str| {
+    let append = |buffer: &mut String, from: &mut usize, to: usize, suffix: &str| {
         // Allocate buffer only when necessary
-        if buffer.is_none() {
+        if buffer.is_empty() {
             // Best case:  length + 1  (i.e., single escaped character in input)
             // Worst case: length * 3  (i.e., every character needs double-escaping)
-            *buffer = Some(String::with_capacity(length * 2));
+            // The input is likely to be short, so no harm in reserving for the worst case
+            buffer.reserve(length * 3);
         }
 
-        if let Some(ref mut buffer) = buffer {
-            // Decant the unescaped chunk from the input,
-            // followed by the escaped suffix provided
-            *buffer += &input[*from..to];
-            *buffer += suffix;
-        }
+        // Decant the unescaped chunk from the input,
+        // followed by the escaped suffix provided
+        *buffer += &input[*from..to];
+        *buffer += suffix;
 
         // Fast-forward the tracking cursor to the next character
         *from = to + 1;
@@ -53,12 +53,12 @@ fn escape(input: &str) -> Cow<str> {
         }
     }
 
-    if let Some(mut buffer) = buffer {
+    if buffer.is_empty() {
+        input.into()
+    } else {
         // Decant whatever's left of the input into the buffer
         buffer += &input[start..length];
         buffer.into()
-    } else {
-        input.into()
     }
 }
 
@@ -96,10 +96,11 @@ pub fn write(output: &mut dyn io::Write, root: &SyntaxNode) -> FormatterResult<(
 #[cfg(test)]
 mod test {
     use super::escape;
+    use std::borrow::Cow;
 
     #[test]
     fn double_escape() {
-        // PBT would be handy, here
+        // Property-based testing would be handy, here...
         assert_eq!(escape("foo"), "foo");
         assert_eq!(escape("'"), r#"\'"#);
         assert_eq!(escape("\n"), r#"\\n"#);
@@ -108,5 +109,21 @@ mod test {
             escape("Here's something\nlonger"),
             r#"Here\'s something\\nlonger"#
         );
+    }
+
+    #[test]
+    fn escape_borrowed() {
+        match escape("foo") {
+            Cow::Borrowed("foo") => (),
+            _ => panic!("Expected a borrowed, unmodified str"),
+        }
+    }
+
+    #[test]
+    fn escape_owned() {
+        match escape("'") {
+            Cow::Owned(s) => assert_eq!(s, r#"\'"#),
+            _ => panic!("Expected an owned, escaped string"),
+        }
     }
 }
