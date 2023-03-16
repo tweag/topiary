23446996eaf0a4ebaf73bcaa4a643cd2295e6380
Tor Hovland <tor.hovland@tweag.io>
2023-03-16 10:18:35 +0100

    Remove unused files.

--- web-playground/react-app/src/App.js
+++ web-playground/react-app/src/App.js
@@ -1,11 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
                 <p>
                     Edit <code>src/App.js</code> and save to reload.
                 </p>
