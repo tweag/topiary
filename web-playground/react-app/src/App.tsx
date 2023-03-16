e28f73cdeedeef2b447d5cf7de0a9684799f5857
Tor Hovland <tor.hovland@tweag.io>
2023-03-16 15:16:15 +0100

    Working playground in React.

--- web-playground/react-app/src/App.tsx
+++ web-playground/react-app/src/App.tsx
@@ -1,28 +1,46 @@
 import { useState } from "react";
 import "./App.css";
+import init, {
+    topiaryInit,
+    format,
+} from "./wasm-app/topiary_playground.js";
 
 function App() {
+    const [isInitialised, setIsInitialised] = useState(false);
+    const [query, setQuery] = useState("");
+    const [input, setInput] = useState("");
     const [output, setOutput] = useState("");
 
-    function format() {
-        setOutput("Formatting ...");
+    async function runFormat() {
+        try {
+            if (!isInitialised) {
+                await init();
+                await topiaryInit();
+                setIsInitialised(true);
+            }
+
+            setOutput("Formatting ...");
+            setOutput(await format(input, query));
+        } catch (e) {
+            setOutput(String(e));
+        }
     }
 
     return (
         <div className="App">
             <div className="header">
-                <button className="btn btn-primary" onClick={format}>
+                <button className="btn btn-primary" onClick={runFormat}>
                     Format
                 </button>
             </div>
             <div className="columns">
                 <div className="column">
                     <h1>Query</h1>
-                    <textarea></textarea>
+                    <textarea value={query} onChange={e => setQuery(e.target.value)} />
                 </div>
                 <div className="column">
                     <h1>Input</h1>
-                    <textarea></textarea>
+                    <textarea value={input} onChange={e => setInput(e.target.value)} />
                 </div>
                 <div className="column">
                     <h1>Output</h1>
