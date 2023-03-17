02f188ad091f6529b848a6c39c4c3a56be722886
Tor Hovland <tor.hovland@tweag.io>
2023-03-17 13:00:54 +0100

    Move to workspace.

--- wasm-app/src/lib.rs
+++ wasm-app/src/lib.rs
@@ -1,6 +1,5 @@
-use std::error::Error;
 use std::panic;
-use topiary::{formatter, Configuration, FormatterError, FormatterResult, Language, Operation};
+use topiary::{formatter, Configuration, FormatterResult, Operation};
 use tree_sitter_facade::TreeSitter;
 use wasm_bindgen::prelude::*;
 
@@ -11,25 +10,27 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen(js_name = topiaryInit)]
-pub async fn topiary_init() {
+pub async fn topiary_init() -> Result<(), JsError> {
     cfg_if::cfg_if! {
         if #[cfg(feature = "console_error_panic_hook")] {
             panic::set_hook(Box::new(console_error_panic_hook::hook));
         }
     }
 
-    TreeSitter::init().await;
+    TreeSitter::init().await.map_err(|e| format_error(&*e))
 }
 
 #[wasm_bindgen]
 pub async fn format(input: &str, query: &str) -> Result<String, JsError> {
-    Ok(format_inner(input, query).await.map_err(format_error)?)
+    format_inner(input, query)
+        .await
+        .map_err(|e| format_error(&e))
 }
 
 async fn format_inner(input: &str, query: &str) -> FormatterResult<String> {
     let mut output = Vec::new();
 
-    let mut configuration = Configuration::parse(&query)?;
+    let configuration = Configuration::parse(query)?;
     let grammars = configuration.language.grammars().await?;
 
     formatter(
@@ -46,9 +47,9 @@ async fn format_inner(input: &str, query: &str) -> FormatterResult<String> {
     Ok(String::from_utf8(output)?)
 }
 
-fn format_error(e: FormatterError) -> JsError {
+fn format_error(e: &dyn std::error::Error) -> JsError {
     let mut message: String = format!("{e}");
-    let mut inner: &dyn std::error::Error = &e;
+    let mut inner: &dyn std::error::Error = e;
 
     while let Some(source) = inner.source() {
         message += &format!("\nCause: {source}");
