b116aae6c1c5ebaadb8d5b95ccdd839905c1f2a5
Nicolas BACQUEY <nicolas.bacquey@tweag.io>
2023-05-16 15:09:51 +0200

    Fix and re-enable benchmark

--- topiary/benches/benchmark.rs
+++ topiary/benches/benchmark.rs
@@ -8,24 +8,24 @@ use topiary::{formatter, Operation};
 async fn format() {
     let input = fs::read_to_string("tests/samples/input/ocaml.ml").unwrap();
     let query = fs::read_to_string("../languages/ocaml.scm").unwrap();
-    // let configuration = Configuration::parse(&query).unwrap();
-    // let grammars = configuration.language.grammars().await.unwrap();
+    let configuration = Configuration::parse_default_config();
+    let language = configuration.get_language("ocaml_implementation");
+    let grammars = language.grammars().await.unwrap();
 
     let mut input = input.as_bytes();
     let mut output = io::BufWriter::new(Vec::new());
 
-    todo!("ERIN")
-    // formatter(
-    //     &mut input,
-    //     &mut output,
-    //     &query,
-    //     &configuration,
-    //     &grammars,
-    //     Operation::Format {
-    //         skip_idempotence: true,
-    //     },
-    // )
-    // .unwrap()
+    formatter(
+        &mut input,
+        &mut output,
+        &query,
+        &language,
+        &grammars,
+        Operation::Format {
+            skip_idempotence: true,
+        },
+    )
+    .unwrap()
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
