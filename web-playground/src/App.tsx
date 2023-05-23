b69e4ee59930ada65eca106460ebd4f70b8e0d54
Tor Hovland <tor.hovland@tweag.io>
2023-05-23 13:23:58 +0200

    Eslint fix: https://eslint.org/docs/latest/rules/no-multi-str

--- web-playground/src/App.tsx
+++ web-playground/src/App.tsx
@@ -41,9 +41,9 @@ function App() {
             let hasModification =
                 input !== languages[currentLanguage].input
                 || query !== languages[currentLanguage].query;
-            let confirmationMessage = "Modifications to the input and query\
- are going to be overwritten if you change the language.\
- Do you wish to proceed?";
+            let confirmationMessage = "Modifications to the input and query " +
+                "are going to be overwritten if you change the language. " +
+                "Do you wish to proceed?";
             if (!hasModification || window.confirm(confirmationMessage)) {
                 setInput(languages[l].input);
                 setQuery(languages[l].query);
