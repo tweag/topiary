47b6c16c3b2e5b60b238a245f21b8456b019d97c
Tor Hovland <tor.hovland@tweag.io>
2023-05-24 11:27:25 +0200

    languages_export.ts as build output (#477)
    * Handle languages_export.ts as a build output.
    
    * Ignore build output.

--- update-wasm-app.sh
+++ update-wasm-app.sh
@@ -3,3 +3,6 @@ nix build .#topiary-playground
 rm -rf web-playground/src/wasm-app
 cp -r result web-playground/src/wasm-app
 chmod -R +w web-playground/src/wasm-app
+rm -rf web-playground/src/samples
+mkdir web-playground/src/samples
+mv web-playground/src/wasm-app/languages_export.ts web-playground/src/samples/
