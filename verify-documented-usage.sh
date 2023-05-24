2658d93a64437516346418a1b486059817e52638
Tor Hovland <tor.hovland@tweag.io>
2023-05-24 10:52:29 +0200

    Update README.md

--- verify-documented-usage.sh
+++ verify-documented-usage.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
-nix run . -- --help |
+nix run . -- --help > usage.txt
+
+cat usage.txt |
 {
     while IFS= read -r line
     do
         grep -Fxq "$line" README.md
         if [ $? -ne 0 ]
         then
-            echo "Usage is not correctly documented in README.md."
+            echo "Usage is not correctly documented in README.md. Update the file with the following:"
+            cat usage.txt
             exit 1
         fi
     done
