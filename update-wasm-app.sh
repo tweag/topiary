01283ad87c044be84067dffb24942ffe12c2463a
Tor Hovland <tor.hovland@tweag.io>
2023-06-05 12:20:25 +0200

    Make sure we copy the contents of the Nix results, not the symlink itself. The frontend build does not like symlinks.

--- update-wasm-app.sh
+++ update-wasm-app.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 nix build .#topiary-playground
 rm -rf web-playground/src/wasm-app
-cp -r result web-playground/src/wasm-app
-chmod -R +w web-playground/src/wasm-app
+mkdir web-playground/src/wasm-app
+cp -r result/* web-playground/src/wasm-app/
 rm -rf web-playground/src/samples
 mkdir web-playground/src/samples
 mv web-playground/src/wasm-app/languages_export.ts web-playground/src/samples/
