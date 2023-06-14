7a114cb32abc0d9ffb4da945eb9cc46a7c636b01
Erin van der Veen <erin.vanderveen@tweag.io>
2023-06-14 12:28:15 +0200

    Look up from $PWD to find the directory containing .topiary

--- topiary-cli/src/configuration.rs
+++ topiary-cli/src/configuration.rs
@@ -1,17 +1,18 @@
 use serde_toml_merge::merge;
-use std::env::current_dir;
+use std::{env::current_dir, path::PathBuf};
 use topiary::{default_configuration_toml, Configuration};
 
 pub fn parse_configuration() -> Configuration {
-    user_lang_toml()
+    user_configuration_toml()
         .expect("TODO: Error")
         .try_into()
         .expect("TODO: Error")
 }
 
 /// User configured languages.toml file, merged with the default config.
-fn user_lang_toml() -> Result<toml::Value, toml::de::Error> {
-    let config = [current_dir().unwrap().join(".topiary")]
+fn user_configuration_toml() -> Result<toml::Value, toml::de::Error> {
+    // TODO(Erin): Error on failure to find workspace
+    let config = [find_workspace().join(".topiary")]
         .into_iter()
         .map(|path| path.join("languages.toml"))
         .filter_map(|file| {
@@ -26,4 +27,17 @@ fn user_lang_toml() -> Result<toml::Value, toml::de::Error> {
         });
 
     Ok(config)
+}
+
+pub fn find_workspace() -> PathBuf {
+    let current_dir = current_dir().expect("Could not get current working directory");
+    for ancestor in current_dir.ancestors() {
+        if ancestor.join(".topiary").exists() {
+            return ancestor.to_owned();
+        }
+    }
+
+    // Default to the current dir if we could not find an ancestor with the .topiary directory
+    // If current_dir does not contain a .topiary, it will be filtered our in the `user_lang_toml` function.
+    current_dir
 }
