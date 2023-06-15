37ab92ae70950b2fac2018ecef777745954235c5
Tor Hovland <tor.hovland@tweag.io>
2023-06-15 11:34:37 +0200

    Rename file to avoid a name collision.

--- topiary/tests/sample-tester.rs
+++ topiary/tests/sample-tester.rs
@@ -121,7 +121,7 @@ async fn exhaustive_query_tester() {
     for file in input_dir {
         let file = file.unwrap();
         // We skip "ocaml.mli", as its query file is already tested by "ocaml.ml"
-        if file.file_name().to_string_lossy() == "ocaml.mli" {
+        if file.file_name().to_string_lossy() == "ocaml-interface.mli" {
             continue;
         }
         let language = Language::detect(file.path(), &config).unwrap();
