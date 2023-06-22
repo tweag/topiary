88b4e7a2d4a72b2a894cff51ac24434e4dfde142
Tor Hovland <tor.hovland@tweag.io>
2023-06-22 09:53:40 +0200

    Sample client app.

---
+++ samples/client-app/src/main.rs
@@ -0,0 +1,29 @@
+use topiary::{formatter, Configuration, Operation, TopiaryQuery};
+
+#[tokio::main]
+async fn main() {
+    let mut input =
+        "{\"name\":\"John Doe\",\"age\":43,\n\"phones\":[\"+44 1234567\",\"+44 2345678\"]}".as_bytes();
+
+    let mut output = Vec::new();
+    let query = TopiaryQuery::json();
+    let config = Configuration::parse_default_configuration().expect("config");
+    let language = config.get_language("json").expect("language");
+    let grammar = language.grammar().await.expect("grammar");
+
+    formatter(
+        &mut input,
+        &mut output,
+        &query,
+        &language,
+        &grammar,
+        Operation::Format {
+            skip_idempotence: false,
+            tolerate_parsing_errors: false,
+        },
+    )
+    .expect("formatter");
+
+    let formatted = String::from_utf8(output).expect("valid utf-8");
+    println!("Here is some formatted JSON:\n{formatted}");
+}
