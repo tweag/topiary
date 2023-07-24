01f863a165713789ec78bea76c81afdc43b6a8e7
Christopher Harrison <christopher.harrison@tweag.io>
2023-07-24 16:51:21 +0100

    Make README/CLI checking script a bit clearer

--- verify-documented-usage.sh
+++ verify-documented-usage.sh
@@ -2,23 +2,39 @@
 
 set -euo pipefail
 
+readonly FENCE='```'
+
 get-cli-usage() {
-  # Get the help text from the CLI and strip trailing whitespace
+  # Get the help text from the CLI
   local subcommand="${1-ROOT}"
 
   case "${subcommand}" in
     "ROOT") nix run . -- --help;;
     *)      nix run . -- "${subcommand}" --help;;
-  esac \
-  | sed 's/\s*$//'
+  esac
 }
 
 get-readme-usage() {
-  # Get the help text from the README and strip trailing whitespace
+  # Get the help text from the README
   local subcommand="${1-ROOT}"
 
-  sed --quiet "/usage:start:${subcommand}/,/usage:end:${subcommand}/ { //!p; }" README.md \
-  | sed --quiet '/```/ !p; s/\s*$//;'
+  sed --quiet "
+    /usage:start:${subcommand}/, /usage:end:${subcommand}/ {
+      //d          # Delete the markers (last pattern)
+      /${FENCE}/d  # Delete the code fences
+      p            # Print anything else
+    }
+  " README.md
+}
+
+diff-usage() {
+  # Generate a diff between the README and CLI help text
+  local subcommand="${1-ROOT}"
+
+  diff --text \
+       --ignore-all-space \
+       <(get-readme-usage "${subcommand}") \
+       <(get-cli-usage "${subcommand}")
 }
 
 main() {
@@ -27,7 +43,7 @@ main() {
   local _diff
   local _subcommand
   for _subcommand in "${subcommands[@]}"; do
-    if ! _diff=$(diff <(get-readme-usage "${_subcommand}") <(get-cli-usage "${_subcommand}")); then
+    if ! _diff=$(diff-usage "${_subcommand}"); then
       >&2 echo "Usage is not correctly documented in README.md for the ${_subcommand} subcommand!"
       echo "${_diff}"
       exit 1
