3e96b37fff66d6b3d819512ceb9ffcbee1228c6d
Tor Hovland <tor.hovland@tweag.io>
2023-07-28 12:20:13 +0200

    Syntax highlighting for all languages except Nickel.

--- web-playground/src/components/editor.tsx
+++ web-playground/src/components/editor.tsx
@@ -1,21 +1,29 @@
 import AceEditor from "react-ace";
 
 import "ace-builds/src-noconflict/ext-language_tools";
-import "ace-builds/src-noconflict/mode-plain_text";
-import "ace-builds/src-noconflict/mode-scheme";
 import "ace-builds/src-noconflict/theme-sqlserver"
+
+import "ace-builds/src-noconflict/mode-json";
+import "ace-builds/src-noconflict/mode-ocaml";
+import "ace-builds/src-noconflict/mode-plain_text";
+import "ace-builds/src-noconflict/mode-rust";
+import "ace-builds/src-noconflict/mode-scheme";
+import "ace-builds/src-noconflict/mode-sh";
+import "ace-builds/src-noconflict/mode-toml";
+
+import "ace-builds/src-noconflict/snippets/sh";
 
 export default function Editor(props: {
     id: string,
     value: string,
     placeholder: string,
-    language: "plain_text" | "scheme";
+    language: string;
     readOnly?: boolean,
     onChange?: (value: string) => void
 }) {
     return (
         <AceEditor
-            mode={props.language}
+            mode={toMode(props.language)}
             theme="sqlserver"
             name={props.id}
             value={props.value}
@@ -32,3 +40,26 @@ export default function Editor(props: {
     )
 }
 
+function toMode(language: string) {
+    switch (language) {
+        case "bash":
+            return "sh";
+        case "json":
+            return "json";
+        case "ocaml":
+        case "ocaml-interface":
+            return "ocaml";
+        case "nickel":
+            // Missing highlighting for Nickel, but we know.
+            return "plain_text";
+        case "rust":
+            return "rust";
+        case "toml":
+            return "toml";
+        case "tree-sitter-query":
+            return "scheme";
+        default:
+            console.warn(`Missing syntax highlighting for ${language}.`);
+            return "plain_text";
+    }
+}
