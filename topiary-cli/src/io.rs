a34353cbffdf8533fb7f452aaba037a9bc9a18d1
Christopher Harrison <christopher.harrison@tweag.io>
2023-08-16 17:34:34 +0100

    WIP: InputFile type

--- topiary-cli/src/io.rs
+++ topiary-cli/src/io.rs
@@ -1,10 +1,42 @@
 use crate::error::CLIResult;
 use std::{
     ffi::OsString,
-    io::{stdout, ErrorKind, Write},
-    path::Path,
+    fs::File,
+    io::{stdin, stdout, ErrorKind, Read, Result, Write},
+    path::{Path, PathBuf},
 };
 use tempfile::NamedTempFile;
+
+enum InputSource {
+    Stdin,
+    Disk(File),
+}
+
+pub struct InputFile {
+    source: InputSource,
+}
+
+impl InputFile {
+    pub fn new(path: &str) -> CLIResult<Self> {
+        Ok(match path {
+            "-" => InputFile {
+                source: InputSource::Stdin,
+            },
+            file => InputFile {
+                source: InputSource::Disk(File::open(file)?),
+            },
+        })
+    }
+}
+
+impl Read for InputFile {
+    fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
+        match self.source {
+            InputSource::Stdin => stdin().lock().read(buf),
+            InputSource::Disk(ref mut file) => file.read(buf),
+        }
+    }
+}
 
 #[derive(Debug)]
 pub enum OutputFile {
@@ -51,16 +83,16 @@ impl OutputFile {
 }
 
 impl Write for OutputFile {
-    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+    fn write(&mut self, buf: &[u8]) -> Result<usize> {
         match self {
-            Self::Stdout => stdout().write(buf),
+            Self::Stdout => stdout().lock().write(buf),
             Self::Disk { staged, .. } => staged.write(buf),
         }
     }
 
-    fn flush(&mut self) -> std::io::Result<()> {
+    fn flush(&mut self) -> Result<()> {
         match self {
-            Self::Stdout => stdout().flush(),
+            Self::Stdout => stdout().lock().flush(),
             Self::Disk { staged, .. } => staged.flush(),
         }
     }
