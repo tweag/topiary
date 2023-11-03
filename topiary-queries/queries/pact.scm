c03209f318f9c889a0474ea34bdd7ba3e5dd7b2e
Christopher Harrison <christopher.harrison@tweag.io>
2023-11-03 10:47:24 +0000

    WIP formatting query

--- topiary-queries/queries/pact.scm
+++ topiary-queries/queries/pact.scm
@@ -1,1 +1,21 @@
-; TODO
+(symbol) @leaf
+
+[
+  (symbol)
+] @append_space @prepend_space
+
+[
+  (list)
+] @allow_blank_line_before
+
+(expression) @append_empty_softline
+
+(list
+  .
+  "(" @append_antispace
+  .
+  (expression) @append_empty_softline @append_indent_start
+
+  ")" @prepend_antispace @prepend_empty_softline @prepend_indent_end
+  .
+)
