99c0ac0b804670571cd9dcb89472a2d0fe8d601f
Erin van der Veen <erin.vanderveen@tweag.io>
2024-01-31 11:41:24 +0100

    sample-app: make buildable and document

--- samples/client-app/src/main.rs
+++ samples/client-app/src/main.rs
@@ -1,30 +1,50 @@
-use topiary_core::{formatter, Configuration, Operation, TopiaryQuery};
+// Import necessary modules
+use topiary_config::Configuration;
+use topiary_core::{formatter, Language, Operation, TopiaryQuery};
 
 #[tokio::main]
 async fn main() {
+    // Define input JSON string
     let mut input =
         "{\"name\":\"John Doe\",\"age\":43,\n\"phones\":[\"+44 1234567\",\"+44 2345678\"]}"
             .as_bytes();
-
     let mut output = Vec::new();
-    let query = TopiaryQuery::json();
-    let config = Configuration::parse_default_configuration().expect("config");
-    let language = config.get_language("json").expect("language");
-    let grammar = language.grammar().await.expect("grammar");
 
+    // Load configuration
+    let config = Configuration::default();
+
+    // Get JSON language configuration
+    let json = config.get_language("json").unwrap();
+
+    // Get default query for JSON
+    let query = topiary_queries::json();
+
+    // Get grammar for JSON language
+    let grammar = json.grammar().unwrap();
+
+    // Create Language struct
+    let language: Language = Language {
+        name: "json".to_owned(),
+        query: TopiaryQuery::new(&grammar, query).unwrap(),
+        grammar,
+        indent: None,
+    };
+
+    // Format the input JSON using the language configuration
     formatter(
         &mut input,
         &mut output,
-        &query,
         &language,
-        &grammar,
         Operation::Format {
             skip_idempotence: false,
             tolerate_parsing_errors: false,
         },
     )
-    .expect("formatter");
+    .unwrap();
 
+    // Convert the formatted output to a string
     let formatted = String::from_utf8(output).expect("valid utf-8");
+
+    // Print the formatted JSON
     println!("Here is some formatted JSON:\n{formatted}");
 }
