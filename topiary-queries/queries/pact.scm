36310d4d51b5cb9c37a6f2ba1bd2a56f8aef2f4e
Christopher Harrison <christopher.harrison@tweag.io>
2024-02-01 17:54:43 +0000

    Beginnings of Pact formatting query
    [skip ci]

--- topiary-queries/queries/pact.scm
+++ topiary-queries/queries/pact.scm
@@ -1,21 +1,58 @@
-(symbol) @leaf
+[
+  (atom)
+  (string)
+] @leaf
 
 [
-  (symbol)
-] @append_space @prepend_space
-
-[
-  (list)
+  (comment)
+  (s_expression)
+  (module)
 ] @allow_blank_line_before
 
-(expression) @append_empty_softline
+[
+  (atom)
+  (comment)
+  (module_identifier)
+  (module_governance)
+  (string)
+] @prepend_space @append_space
 
-(list
-  .
+(comment) @append_hardline
+
+; Remove spaces following opening/preceding closing parens in all sexprs
+(
   "(" @append_antispace
-  .
-  (expression) @append_empty_softline @append_indent_start
+  ")" @prepend_antispace
+)
 
-  ")" @prepend_antispace @prepend_empty_softline @prepend_indent_end
-  .
+;; Indentation
+
+; We opt for idiomatic Lisp indentation of sexprs: The indentation
+; block is opened after a context-specific atom in the sexpr (not
+; necessarily the head element); then the closing paren falls on the
+; last line of the sexpr, rather than having a line of its own.
+; Something like this:
+;
+;   (foo bar
+;      (quux xyzzy))
+;
+; Opposed to Algol-style formatting:
+;
+;   (
+;     foo bar
+;     (quux xyzzy)
+;   )
+
+; Modules
+; Start the indentation block after the (module_governance) node
+(module
+  (module_governance) @append_spaced_softline @append_indent_start
+  ")" @append_indent_end
+)
+
+; S-Expressions
+; Start the indentation block after the head node
+(s_expression
+  (s_expression_head) @append_spaced_softline @append_indent_start
+  ")" @append_indent_end
 )
