961cdf780d0244e99be579402de49fce7a9b90ee
Mikhail Katychev <mkatych@gmail.com>
2025-01-09 12:48:21 -0600

    handled unnested function formatting

--- topiary-cli/tests/samples/expected/openscad.scad
+++ topiary-cli/tests/samples/expected/openscad.scad
@@ -45,51 +45,48 @@ module big_module() {
   module inner_module() cube();
 }
 
-// module extern_module() include <other_file.scad>
-//
-// for (i = [10:50])
-// {
-//     let (angle = i*360/20, r= i*2, distance = r*5)
-//     {
-//         rotate(angle, [1, 0, 0])
-//         translate([0, distance, 0])
-//         sphere(r = r);
-//     }
-// }
-//
-// if ($preview)
-//     if (false)
-//         sphere();
-//     else
-//         translate([2, 0, 0]) cube();
-//
-// for (i = [1:2:7]) {
-//     let (x = i ^ 2, y = x - 1) {
-//         translate([x, y, 0]) sphere(r = i);
-//     }
-// }
-//
-// intersection_for (i = [1, 2, 3]) {
-//     if (i > 1) {
-//         translate([0, i, 0]) cube();
-//     }
-// }
-//
-// // ================================================================================
-// // Comments
-// // ================================================================================
-// /* ignored [Customizer Group] ignored */
-// /* Multiline
-// comment
-// here
-// */
-// my_parameter = 5;
-//
-// function math(x) = /*do math stuff*/ x + 2 // done with math
-// ;
-//
-// module my_cylinder() {
-//   // here we create a cylinder
-//   cylinder(); /* done ! */
-//   cube();
-// }
+module extern_module() include <other_file.scad>
+
+for (i = [10:50])
+{
+    let (angle = i*360/20, r= i*2, distance = r*5)
+    {
+        rotate(angle, [1, 0, 0])
+        translate([0, distance, 0])
+        sphere(r = r);
+    }
+}
+
+if ($preview)
+    if (false)
+        sphere();
+    else
+        translate([2, 0, 0]) cube();
+
+for (i = [1:2:7]) {
+    let (x = i ^ 2, y = x - 1) {
+        translate([x, y, 0]) sphere(r = i);
+    }
+}
+
+intersection_for (i = [1, 2, 3]) {
+    if (i > 1) {
+        translate([0, i, 0]) cube();
+    }
+}
+
+// ================================================================================
+// Comments
+// ================================================================================
+/* ignored [Customizer Group] ignored */
+my_parameter = 5;
+
+function math(x) = /*do math stuff*/ x + 2 // done with math
+;
+
+module my_cylinder() {
+  // here we create a cylinder
+  cylinder(); /* done ! */
+  cube();
+}
+
--- topiary-queries/queries/openscad.scm
+++ topiary-queries/queries/openscad.scm
@@ -1,3 +1,129 @@
-(include_statement) @append_empty_softline
-(assignment) @prepend_empty_softline
-(identifier) @append_space
+; Sometimes we want to indicate that certain parts of our source text should
+; not be formatted, but taken as is. We use the leaf capture name to inform the
+; tool of this.
+[
+  (block_comment)
+  (line_comment)
+  (string)
+] @leaf
+
+; Allow blank line before
+[
+  (use_statement)
+  (include_statement)
+  (block_comment)
+  (line_comment)
+  (function_item)
+  (transform_chain)
+  (module_declaration)
+  (expression)
+] @allow_blank_line_before
+
+; Keywords
+
+; Surround spaces
+[
+  "module"
+  "function"
+  "let"
+  "include"
+  "assign"
+  "use"
+  "each"
+  "||"
+  "&&"
+  "=="
+  "!="
+  "<"
+  ">"
+  "<="
+  ">="
+  "+"
+  "-"
+  "*"
+  "/"
+  "%"
+  "^"
+  "!"
+  "="
+  "?"
+  ":"
+] @prepend_space @append_space
+
+; Colon should have whitespace trimmed in a range delimiter
+(range ":" @prepend_antispace @append_antispace)
+
+; Input softlines before and after all comments. This means that the input
+; decides if a comment should have line breaks before or after. A line comment
+; always ends with a line break.
+[
+  (block_comment)
+  (line_comment)
+] @prepend_input_softline
+
+; Append line breaks. If there is a comment following, we don't add anything,
+; because the input softlines and spaces above will already have sorted out the
+; formatting.
+(
+  [
+    (assignment)
+    (use_statement)
+    (include_statement)
+    (function_item)
+    (module_declaration)
+    (transform_chain)
+  ] @append_spaced_softline
+  .
+  [
+    (block_comment)
+    (line_comment)
+  ]* @do_nothing
+)
+
+(line_comment) @append_hardline
+
+(block_comment) @multi_line_indent_all
+
+; Allow line break after block comments
+(
+  (block_comment)
+  .
+  _ @prepend_input_softline
+)
+
+; indent the body of a function
+(function_item
+  (parameters_declaration)
+  .
+  "=" @append_spaced_softline @append_indent_start
+  (expression)
+  ";" @prepend_indent_end
+)
+
+; Append softlines, unless followed by comments.
+(
+  [
+    ","
+    ";"
+  ] @append_spaced_softline
+  .
+  [(block_comment) (line_comment)]* @do_nothing
+)
+
+; Never put a space before a comma
+(
+  "," @prepend_antispace
+)
+
+; Don't insert spaces between the operator and their expression operand
+(unary_expression
+  _ @append_antispace
+  .
+  (expression)
+)
+
+; Don't insert spaces between the starting '[' and ending ']' of an index expression
+(index_expression
+  value: _ @append_antispace
+  index: _ @prepend_antispace @append_antispace
+)
